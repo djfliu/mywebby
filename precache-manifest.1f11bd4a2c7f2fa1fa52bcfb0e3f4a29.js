da08e22be2c7fd97d1bcce619420ec58ac397d53
djfliu <danny98liu@gmail.com>
2019-10-12 14:13:33 -0400

    Updates

---
+++ precache-manifest.1f11bd4a2c7f2fa1fa52bcfb0e3f4a29.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0434761353c0f23f42f688e6380532ef",
+    "url": "/mywebby/index.html"
+  },
+  {
+    "revision": "b8d4741965fb72a4452c",
+    "url": "/mywebby/static/css/main.b1b50c53.chunk.css"
+  },
+  {
+    "revision": "9f073f636ba2e8dc9162",
+    "url": "/mywebby/static/js/2.575fd95a.chunk.js"
+  },
+  {
+    "revision": "b8d4741965fb72a4452c",
+    "url": "/mywebby/static/js/main.5c732800.chunk.js"
+  },
+  {
+    "revision": "27b858da3bb0f8ea5671",
+    "url": "/mywebby/static/js/runtime~main.9c2b2098.js"
+  },
+  {
+    "revision": "738e9950b7c090ddaa37fe3f99a49cdf",
+    "url": "/mywebby/static/media/church.738e9950.jpg"
+  },
+  {
+    "revision": "7356b1c515cffb734fee89b9d5a269a9",
+    "url": "/mywebby/static/media/landscape.7356b1c5.jpg"
+  },
+  {
+    "revision": "d47b238c1f6bbc35c1514fa531c2e35b",
+    "url": "/mywebby/static/media/me.d47b238c.jpg"
+  },
+  {
+    "revision": "a31dc33768efc50717fad2807202dacf",
+    "url": "/mywebby/static/media/palace.a31dc337.jpg"
+  },
+  {
+    "revision": "2b0f22bf24ff14b440a02701711d4e79",
+    "url": "/mywebby/static/media/reflection.2b0f22bf.jpg"
+  },
+  {
+    "revision": "07e81265493348f2d87945da5280206d",
+    "url": "/mywebby/static/media/tonality-thumbnnail.07e81265.png"
+  },
+  {
+    "revision": "ef07da4010857e9580ed72e4515eaf63",
+    "url": "/mywebby/static/media/wizzair.ef07da40.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mywebby/precache-manifest.1f11bd4a2c7f2fa1fa52bcfb0e3f4a29.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mywebby/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
